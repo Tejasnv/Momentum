fdf82c7cd05c1793d75b15c299c57fec0850aa4e
Tejas <tejasvarsekar@gmail.com>
2026-09-25 11:29:38 -0700

    added predeploy

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { MomentPage, PlannerProvider } from "./features/planner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <PlannerProvider>
         <Routes>
           <Route path="/" element={<App />} />
